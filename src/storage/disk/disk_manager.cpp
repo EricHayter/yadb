d5c5e1e9c5a32d5dcaa8f529c095aae5ad6674ed
Eric <hayter.eric@gmail.com>
2025-05-25 14:18:08 -0400

    format code

--- src/storage/disk/disk_manager.cpp
+++ src/storage/disk/disk_manager.cpp
@@ -1,8 +1,7 @@
 #include "storage/disk/disk_manager.h"
 
-DiskManager::DiskManager(const std::filesystem::path &db_file)
+DiskManager::DiskManager(const std::filesystem::path& db_file)
 {
-	db_io_m = std::fstream(db_file);
-	// TODO check to see if the open fails
+    db_io_m = std::fstream(db_file);
+    // TODO check to see if the open fails
 }
-
