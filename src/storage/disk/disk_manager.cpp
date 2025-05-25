1cb6386b51f59fc6b561c085af3767511953f634
Eric <hayter.eric@gmail.com>
2025-05-25 14:16:46 -0400

    Implementing disk manager to work with pages

---
+++ src/storage/disk/disk_manager.cpp
@@ -0,0 +1,8 @@
+#include "storage/disk/disk_manager.h"
+
+DiskManager::DiskManager(const std::filesystem::path &db_file)
+{
+	db_io_m = std::fstream(db_file);
+	// TODO check to see if the open fails
+}
+
