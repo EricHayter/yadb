0727c428a7c3097b3549b653a006744854561b38
Eric <hayter.eric@gmail.com>
2025-05-28 07:34:28 -0400

    move definition of DiskData to common definitions file

--- include/common/type_definitions.h
+++ include/common/type_definitions.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <cstdint>
+#include <span>
 
 constexpr std::size_t PAGE_SIZE = 4096;
 
 using page_id_t = uint32_t;
 using frame_id_t = uint32_t;
+
+using PageData = std::span<char, PAGE_SIZE>;
