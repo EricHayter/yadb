1bf7718816b44a5544557877c8940eeadc6dc1bb
Eric <hayter.eric@gmail.com>
2025-05-27 20:28:42 -0400

    Format code

--- src/storage/disk/disk_manager.cpp
+++ src/storage/disk/disk_manager.cpp
@@ -11,50 +11,48 @@ DiskManager::DiskManager(const std::filesystem::path& db_file)
 
 DiskManager::~DiskManager()
 {
-	db_io_m.close();
+    db_io_m.close();
 }
 
 // maybe look returning an error code
 void DiskManager::WritePage(page_id_t page_id, PageData page_data)
 {
-	std::size_t offset;
-	if (offset_map_m.contains(page_id)) {
-		offset = offset_map_m[page_id];
-	// TODO should this really be how I am handling it?
-	} else {
-		offset = AllocatePage();
-		offset_map_m[page_id] = offset;
-	}
-	
-	db_io_m.seekg(offset);
-	db_io_m.write(page_data.data(), page_data.size());
+    std::size_t offset;
+    if (offset_map_m.contains(page_id)) {
+        offset = offset_map_m[page_id];
+        // TODO should this really be how I am handling it?
+    } else {
+        offset = AllocatePage();
+        offset_map_m[page_id] = offset;
+    }
+
+    db_io_m.seekg(offset);
+    db_io_m.write(page_data.data(), page_data.size());
 }
 
 // Read the contents of page data into page_data
 void DiskManager::ReadPage(page_id_t page_id, PageData page_data)
 {
-	assert(offset_map_m.contains(page_id));
-	size_t offset = offset_map_m[page_id];
-	db_io_m.seekg(offset);
-	db_io_m.read(page_data.data(), page_data.size());
+    assert(offset_map_m.contains(page_id));
+    size_t offset = offset_map_m[page_id];
+    db_io_m.seekg(offset);
+    db_io_m.read(page_data.data(), page_data.size());
 
-	// check the codes to see if it failed...
+    // check the codes to see if it failed...
 }
-
 
 std::size_t DiskManager::AllocatePage()
 {
-	size_t offset;
-	if (not free_pages_m.empty())
-	{
-		offset = free_pages_m.back();
-		free_pages_m.pop_back();
-	} else {
-		if (offset_map_m.size() >= page_capacity_m) {
-        	page_capacity_m *= 2;
-            std::filesystem::resize_file(db_file_path_m, (page_capacity_m) * PAGE_SIZE);
+    size_t offset;
+    if (not free_pages_m.empty()) {
+        offset = free_pages_m.back();
+        free_pages_m.pop_back();
+    } else {
+        if (offset_map_m.size() >= page_capacity_m) {
+            page_capacity_m *= 2;
+            std::filesystem::resize_file(db_file_path_m, (page_capacity_m)*PAGE_SIZE);
         }
-		offset = offset_map_m.size() * PAGE_SIZE;
-	}
-	return offset;
+        offset = offset_map_m.size() * PAGE_SIZE;
+    }
+    return offset;
 }
