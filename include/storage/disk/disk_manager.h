e26c62ea2523ab64e6c89e3a7bca1b49fd5a22f1
Eric <hayter.eric@gmail.com>
2025-05-30 16:15:21 -0400

    Add delete page function for disk manager and reuse page on realloc

--- include/storage/disk/disk_manager.h
+++ include/storage/disk/disk_manager.h
@@ -10,6 +10,7 @@
 class DiskManager {
 public:
     DiskManager(const std::filesystem::path& db_file);
+    DiskManager(const std::filesystem::path& db_file, std::size_t page_capacity);
     ~DiskManager();
 
     // Write data to page to given id
@@ -18,10 +19,12 @@ public:
     // Read contents of entire page
     void ReadPage(page_id_t page_id, PageData page_data);
 
+	void DeletePage(page_id_t page_id);
+
 private:
     std::size_t AllocatePage();
 
-    std::size_t page_capacity_m { 8 };
+    std::size_t page_capacity_m;
 
     // map page id's to offsets in the database file
     std::unordered_map<page_id_t, std::size_t> offset_map_m;
@@ -30,5 +33,5 @@ private:
     std::list<std::size_t> free_pages_m;
 
     std::fstream db_io_m;
-    std::filesystem::path db_file_path_m;
+    std::filesystem::path db_file_m;
 };
--- src/storage/disk/disk_manager.cpp
+++ src/storage/disk/disk_manager.cpp
@@ -3,10 +3,27 @@
 #include <cassert>
 #include <filesystem>
 
+
 DiskManager::DiskManager(const std::filesystem::path& db_file)
+	: DiskManager(db_file, 1)
 {
-    db_io_m = std::fstream(db_file);
-    // TODO check to see if the open fails
+}
+
+DiskManager::DiskManager(const std::filesystem::path& db_file, std::size_t page_capacity)
+    : db_file_m(db_file)
+    , page_capacity_m(page_capacity)
+{
+	// TODO worry about persistence later maybe?
+    db_io_m.open(db_file_m, std::ios::in | std::ios::out | std::ios::binary | std::ios::trunc);
+    assert(db_io_m.is_open());
+
+	std::filesystem::resize_file(db_file_m, page_capacity_m * PAGE_SIZE);
+	assert(page_capacity * PAGE_SIZE == std::filesystem::file_size(db_file_m));
+	for (int i = 0; i < page_capacity_m; i++)
+	{
+		free_pages_m.push_back(i * PAGE_SIZE);
+	}
+
 }
 
 DiskManager::~DiskManager()
@@ -20,14 +37,15 @@ void DiskManager::WritePage(page_id_t page_id, PageData page_data)
     std::size_t offset;
     if (offset_map_m.contains(page_id)) {
         offset = offset_map_m[page_id];
-        // TODO should this really be how I am handling it?
     } else {
+        // TODO should this really be how I am handling it?
         offset = AllocatePage();
         offset_map_m[page_id] = offset;
     }
 
     db_io_m.seekg(offset);
     db_io_m.write(page_data.data(), page_data.size());
+	assert(db_io_m.good());
 }
 
 // Read the contents of page data into page_data
@@ -41,16 +59,30 @@ void DiskManager::ReadPage(page_id_t page_id, PageData page_data)
     // check the codes to see if it failed...
 }
 
+void DiskManager::DeletePage(page_id_t page_id)
+{
+	if (not offset_map_m.contains(page_id))
+		return;
+
+	std::size_t offset = offset_map_m[page_id];
+	offset_map_m.erase(page_id);
+	free_pages_m.push_back(offset);
+}
+
 std::size_t DiskManager::AllocatePage()
 {
     size_t offset;
     if (not free_pages_m.empty()) {
         offset = free_pages_m.back();
         free_pages_m.pop_back();
-    } else {
-        if (offset_map_m.size() >= page_capacity_m) {
+	} else if (page_capacity_m == 0) {
+		page_capacity_m = 1;
+		std::filesystem::resize_file(db_file_m, PAGE_SIZE);
+		offset = 0;
+    } else if (page_capacity_m > 0){
+        if (offset_map_m.size() == page_capacity_m) {
             page_capacity_m *= 2;
-            std::filesystem::resize_file(db_file_path_m, (page_capacity_m)*PAGE_SIZE);
+            std::filesystem::resize_file(db_file_m, page_capacity_m * PAGE_SIZE);
         }
         offset = offset_map_m.size() * PAGE_SIZE;
     }
