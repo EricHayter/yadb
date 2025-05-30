66d021000514b2ea624c0c838136f44016d85523
Eric <hayter.eric@gmail.com>
2025-05-30 09:19:37 -0400

    finish implementing the disk scheduler

--- include/storage/disk/disk_scheduler.h
+++ include/storage/disk/disk_scheduler.h
@@ -1,34 +1,33 @@
 #pragma once
 #include "common/type_definitions.h"
 #include "storage/disk/disk_manager.h"
-#include <future>
-#include <queue>
-#include <optional>
-#include <thread>
 #include <condition_variable>
+#include <filesystem>
+#include <future>
+#include <optional>
+#include <queue>
 #include <stop_token>
+#include <thread>
 
-struct DiskIOTask
-{
-	bool is_write;
-	PageData data;
-	page_id_t page_id;
-	std::promise<bool> callback; // maybe change this to a enum
+struct DiskIOTask {
+    bool is_write;
+    PageData data;
+    page_id_t page_id;
+    std::promise<bool> callback; // maybe change this to a enum
 };
 
-class DiskScheduler
-{
-	public:
-	DiskScheduler();
-	~DiskScheduler();
-	void Enqueue(const DiskIOTask& task);
+class DiskScheduler {
+public:
+    DiskScheduler(const std::filesystem::path& db_path);
+    ~DiskScheduler();
+    void Enqueue(const DiskIOTask& task);
 
-	private:
-	void WorkerFunction(std::stop_token st);
+private:
+    void WorkerFunction(std::stop_token st);
 
-	std::jthread worker_thread_m;	
-	std::condition_variable cv_m;
-	std::mutex mut_m;
-	std::queue<DiskIOTask> tasks_m;
-	DiskManager disk_manager_m;
+    std::jthread worker_thread_m;
+    std::condition_variable cv_m;
+    std::mutex mut_m;
+    std::queue<DiskIOTask> tasks_m;
+    DiskManager disk_manager_m;
 };
--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -1,52 +1,47 @@
 #include "storage/disk/disk_scheduler.h"
+#include "storage/disk/disk_manager.h"
 #include <mutex>
 #include <thread>
 
-DiskScheduler::DiskScheduler()
+DiskScheduler::DiskScheduler(const std::filesystem::path& db_path)
+    : disk_manager_m(db_path)
+    , worker_thread_m(&DiskScheduler::WorkerFunction, this)
 {
-	worker_thread_m = std::jthread(&DiskScheduler::WorkerFunction, this);
 }
 
 DiskScheduler::~DiskScheduler()
 {
-	worker_thread_m.request_stop();
+    worker_thread_m.request_stop();
 }
-
 
 void DiskScheduler::Enqueue(const DiskIOTask& task)
 {
-	std::lock_guard<std::mutex> lg(mut_m);
-	tasks_m.push(task);
-	cv_m.notify_one();
+    std::lock_guard<std::mutex> lg(mut_m);
+    tasks_m.push(task);
+    cv_m.notify_one();
 }
-
 
 void DiskScheduler::WorkerFunction(std::stop_token st)
 {
-	while (not st.stop_requested())
-	{
-		std::unique_lock<std::mutex> lk(mut_m);
-		cv_m.wait(lk, [this](){ return not tasks_m.empty(); });
+    while (not st.stop_requested()) {
+        std::unique_lock<std::mutex> lk(mut_m);
+        cv_m.wait(lk, [this]() { return not tasks_m.empty(); });
 
-		while (not tasks_m.empty())
-		{
-			DiskIOTask task = std::move(tasks_m.front());
-			tasks_m.pop();
-			lk.unlock();
+        while (not tasks_m.empty()) {
+            DiskIOTask task = std::move(tasks_m.front());
+            tasks_m.pop();
+            lk.unlock();
 
-			if (task.is_write)
-			{
-				disk_manager_m.WritePage(task.page_id, task.data);
-			}
-			else
-			{
-				disk_manager_m.ReadPage(task.page_id, task.data);
-			}
+            if (task.is_write) {
+                disk_manager_m.WritePage(task.page_id, task.data);
+            } else {
+                disk_manager_m.ReadPage(task.page_id, task.data);
+            }
 
-			// TODO this should have much better error handling
-			// give an enum instead of bool
-			task.callback.set_value(true);
-			lk.lock();
-		}
-	}
+            // TODO this should have much better error handling
+            // give an enum instead of bool
+            task.callback.set_value(true);
+            lk.lock();
+        }
+    }
 }
