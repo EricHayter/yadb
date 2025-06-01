f555da49d695ae619de492c841ba3de12b9b0c4f
Eric <hayter.eric@gmail.com>
2025-06-01 07:33:17 -0400

    add definition for immutable page data

--- include/common/type_definitions.h
+++ include/common/type_definitions.h
@@ -8,4 +8,8 @@ constexpr std::size_t PAGE_SIZE = 4096;
 using page_id_t = uint32_t;
 using frame_id_t = uint32_t;
 
+// memory views for page data
 using PageData = std::span<char, PAGE_SIZE>;
+
+// imutable view on page data
+using ImutPageData = std::span<const char, PAGE_SIZE>;
