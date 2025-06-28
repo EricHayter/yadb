addbbf3446a554cc22184eb485b5abc490cf3e71
Eric <hayter.eric@gmail.com>
2025-06-28 09:19:02 -0400

    Add documentation for frame header

--- include/buffer/frame_header.h
+++ include/buffer/frame_header.h
@@ -2,6 +2,19 @@
 #include "common/type_definitions.h"
 #include <shared_mutex>
 
+/**
+ * @brief Class to manage state of frames inside of the page buffer manager
+ *
+ * This class specifically tracks two important pieces of information for each
+ * frame:
+ * 1. page id: the page id of the page being contained in the frame
+ * 2. "dirtiness": whether or not a frame has been written to (if that's the
+ *    case then the frame data must be written to disk when the page is
+ *    released from the frame)
+ * 3. pin count: tracking how many accessors there are for this particular
+ *    frame. Not only indicates when a frame is in use but also if a writable
+ *    view can be created for the frame.
+ */
 struct FrameHeader {
     FrameHeader(frame_id_t id, MutPageView data_view);
 
@@ -14,15 +27,19 @@ struct FrameHeader {
     PageView GetData() { return data; };
 
     frame_id_t id;
+
+    // A mutable view into the buffer provided by the page buffer manager.
     MutPageView data;
+
+    /// The associated page that the frame is storing.
     page_id_t page_id;
 
-    // Has this page been altered?
+    /// Has this page been written to?
     bool is_dirty { false };
 
-    // concurrent readers/writers
+    /// concurrent readers/writers
     int pin_count { 0 };
 
-    // mutex for safely sharing access to the buffer
+    /// for concurrent readers and single writers
     std::shared_mutex mut;
 };
