9da46979ab0991e9ebf8743de2e3e8a135f46b95
Eric <hayter.eric@gmail.com>
2025-06-29 07:35:59 -0400

    Add documentation for io tasks

--- include/storage/disk/io_tasks.h
+++ include/storage/disk/io_tasks.h
@@ -5,24 +5,36 @@
 
 namespace IOTasks {
 
+/**
+ * @brief Struct to represent a task for creating a page
+ */
 struct AllocatePageTask {
     std::promise<page_id_t> result;
 };
 
+/**
+ * @brief Struct to represent a task for deleting a page
+ */
 struct DeletePageTask {
     page_id_t page_id;
     std::promise<void> done;
 };
 
+/**
+ * @brief Struct to represent a task for writing to a page
+ */
 struct WritePageTask {
     page_id_t page_id;
-    PageView data;
+    PageView data; /// span containing data to write to page
     std::promise<bool> status;
 };
 
+/**
+ * @brief Struct to represent a task for reading from a page
+ */
 struct ReadPageTask {
     page_id_t page_id;
-    MutPageView data;
+    MutPageView data; /// span to write data from read into
     std::promise<bool> status;
 };
 
