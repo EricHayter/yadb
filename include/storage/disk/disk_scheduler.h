da3066f42c56ddacedf08a58bc86d65a686eef7d
Eric <hayter.eric@gmail.com>
2025-06-29 07:33:05 -0400

    Add documentation for the disk scheduler

--- include/storage/disk/disk_scheduler.h
+++ include/storage/disk/disk_scheduler.h
@@ -10,7 +10,9 @@
 #include <stop_token>
 #include <thread>
 
-/* Disk Scheduler
+/**
+ * @brief Disk Scheduler
+ *
  * The disk scheduler allows for asynchronous operations to the disk manager
  * to be performed. The scheduler consists of a task queue and a worker thread
  * to perform each of the types of operations. The public interface includes
@@ -22,21 +24,53 @@ public:
     DiskScheduler(const std::filesystem::path& db_file);
     ~DiskScheduler();
 
+    /**
+     * @brief Request the creation of a new page
+     *
+     * @param result a promise containing the newly created page
+     */
     void AllocatePage(std::promise<page_id_t>&& result);
+
+    /**
+     * @brief Request to delete page
+     *
+     * @param done a promise indicating when the action is complete
+     */
     void DeletePage(page_id_t page_id, std::promise<void>&& done);
+
+    /**
+     * @brief Request to read a page
+     *
+     * @param page_id the page to read from
+     * @param data the span to write the data into
+     * @param status a promise containing the success status of the read
+     */
     void ReadPage(page_id_t page_id, MutPageView data, std::promise<bool>&& status);
+
+    /**
+     * @brief Request to write to a page
+     *
+     * @param page_id the page to write to
+     * @param data the span containing the data to write to the page
+     * @param status a promise containing the success status of the write
+     */
     void WritePage(page_id_t page_id, PageView data, std::promise<bool>&& status);
 
 private:
+    /**
+     * @brief Worker thread function to handle requests
+     *
+     * @param stop_token stop token for stopping worker thread
+     */
     void WorkerFunction(std::stop_token stop_token);
     std::jthread worker_thread_m;
 
-    // thread-safe task queue with a condition variable to notify worker thread
-    // of task arrival
+    /// thread-safe task queue with a condition variable to notify worker thread
+    /// of task arrival
     std::condition_variable cv_m;
     std::mutex mut_m;
     std::queue<IOTasks::Task> tasks_m;
 
-    // underlying disk manager for worker thread to perform I/O operations
+    /// underlying disk manager for worker thread to perform I/O operations
     DiskManager disk_manager_m;
 };
