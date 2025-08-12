4fc32be1eff4b430affdf85b9878bb318f9fb407
Eric <hayter.eric@gmail.com>
2025-08-11 21:13:50 -0400

    Begin stubbing out what the slotted page implementation is going to look like
    This is the start of the implemenation for the slotted page format. At a high level the buffer pool manager (hopefully) should be refactored to pass these logical pages instead of the guards themselves. TBD whether or not the page guard will just be placed directly inside of the pages themeselves or they will be combined into a single object. This will require a pretty big refactor especially in the lower level tests I assume.

--- include/common/type_definitions.h
+++ include/common/type_definitions.h
@@ -3,18 +3,5 @@
 #include <cstdint>
 #include <span>
 
-constexpr std::size_t PAGE_SIZE = 4096;
-
 using page_id_t = uint32_t;
 using frame_id_t = uint32_t;
-
-// view/span into a page-sized buffer. Immutable by default so I don't blow my
-// leg off by accidentally handling a mutable view.
-using MutPageView = std::span<char, PAGE_SIZE>;
-using PageView = std::span<const char, PAGE_SIZE>;
-
-enum class PageType {
-    Data,
-    BPTreeInner,
-    BPTreeLeaf
-};
