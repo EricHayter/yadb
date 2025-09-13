72715d4a1ac54fb486224422c339d1e98614b98f
Eric <hayter.eric@gmail.com>
2025-09-13 13:06:10 -0400

    Implement simple factory methods for simple loggers

--- include/config/config.h
+++ include/config/config.h
@@ -3,9 +3,16 @@
 #include "spdlog/logger.h"
 #include <filesystem>
 #include <memory>
+#include <string>
 
 struct DatabaseConfig {
-    std::filesystem::path database_file { "data.db" };
+    static inline const std::filesystem::path DEFAULT_DATABASE_FILE { "data.db" };
+
+    static inline const std::string DEFAULT_DISK_MANAGER_LOGGER_NAME { "disk_manager" };
+    static inline const std::string DEFAULT_DISK_SCHEDULER_LOGGER_NAME { "disk_scheduler" };
+    static inline const std::string DEFAULT_PAGE_BUFFER_MANAGER_LOGGER_NAME { "page_buffer_manager" };
+
+    std::filesystem::path database_file { DEFAULT_DATABASE_FILE };
 
     std::shared_ptr<spdlog::logger> disk_manager_logger;
     std::shared_ptr<spdlog::logger> disk_scheduler_logger;
---
+++ src/config/config.cpp
@@ -0,0 +1,28 @@
+#include "config/config.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include <memory>
+
+DatabaseConfig DatabaseConfig::CreateDefaultConsole() {
+    DatabaseConfig config;
+    config.disk_manager_logger = spdlog::stderr_color_mt(DEFAULT_DISK_MANAGER_LOGGER_NAME);
+    config.disk_scheduler_logger = spdlog::stderr_color_mt(DEFAULT_DISK_SCHEDULER_LOGGER_NAME);
+    config.page_buffer_manager_logger = spdlog::stderr_color_mt(DEFAULT_PAGE_BUFFER_MANAGER_LOGGER_NAME);
+    return config;
+}
+
+DatabaseConfig DatabaseConfig::CreateDefaultFile() {
+    DatabaseConfig config;
+    config.disk_manager_logger = spdlog::basic_logger_mt(DEFAULT_DISK_MANAGER_LOGGER_NAME, DEFAULT_DISK_MANAGER_LOGGER_NAME + ".log");
+    config.disk_scheduler_logger = spdlog::basic_logger_mt(DEFAULT_DISK_SCHEDULER_LOGGER_NAME, DEFAULT_DISK_SCHEDULER_LOGGER_NAME + ".log");
+    config.page_buffer_manager_logger = spdlog::basic_logger_mt(DEFAULT_PAGE_BUFFER_MANAGER_LOGGER_NAME, DEFAULT_PAGE_BUFFER_MANAGER_LOGGER_NAME + ".log");
+    return config;
+}
+
+DatabaseConfig DatabaseConfig::CreateNull() {
+    DatabaseConfig config;
+    config.disk_manager_logger = std::make_shared<spdlog::logger>(DEFAULT_DISK_MANAGER_LOGGER_NAME);
+    config.disk_scheduler_logger = std::make_shared<spdlog::logger>(DEFAULT_DISK_SCHEDULER_LOGGER_NAME);
+    config.page_buffer_manager_logger = std::make_shared<spdlog::logger>(DEFAULT_PAGE_BUFFER_MANAGER_LOGGER_NAME);
+    return config;
+}
