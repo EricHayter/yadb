bf9c52235008a9f0ae26c93911ce05f18a6c07dc
Eric <hayter.eric@gmail.com>
2025-09-20 23:22:06 -0400

    Add test for integrity after vacuum operation
    Also added a allocate function although it's being test in other spots

--- tests/buffer/page_buffer_manager_test.cpp
+++ tests/buffer/page_buffer_manager_test.cpp
@@ -19,6 +19,26 @@ TEST(PageBufferManagerTest, TestPageInit)
     ASSERT_GT(page.GetFreeSpaceSize(), 0);
 }
 
+TEST(PageBufferManagerTest, TestAllocateSlot)
+{
+    int number_frames = 1;
+    PageBufferManager page_buffer_man(number_frames);
+
+    page_id_t page_id = page_buffer_man.NewPage();
+    MutPage page = page_buffer_man.WritePage(page_id);
+
+    EXPECT_EQ(page.GetPageId(), page_id);
+    EXPECT_EQ(page.GetNumSlots(), 0);
+
+    EXPECT_GT(page.GetFreeSpaceSize(), 0);
+
+    constexpr int data_size = 4;
+    auto slot = page.AllocateSlot(data_size);
+
+    EXPECT_EQ(page.GetNumSlots(), 1);
+}
+
+
 TEST(PageBufferManagerTest, TestPageReadWrite)
 {
     /* prepare some mock data */
@@ -213,7 +233,7 @@ TEST(PageBufferManagerTest, TestFlushPage)
     ASSERT_EQ(page.GetFreeSpaceSize() - num_slots * data_size, free_space_size);
 }
 
-TEST(PageBufferManagerTest, TestFlushPageNoReusableSpace)
+TEST(PageBufferManagerTest, TestVacuumPageNoReusableSpace)
 {
     constexpr int number_frames = 1;
     PageBufferManager page_buffer_man(number_frames);
@@ -241,8 +261,14 @@ TEST(PageBufferManagerTest, TestFlushPageNoReusableSpace)
     ASSERT_EQ(page.GetFreeSpaceSize(), free_space_size);
 }
 
-TEST(PageBufferManagerTest, TestFlushPageDeletedInnerSlot)
+TEST(PageBufferManagerTest, TestVacuumPageMiddleInnerSlot)
 {
+    /* When deleting a slot all subsequent slots to the left (lower offset) in
+     * the page should be shifted to the right (higher offset) to the end of
+     * the page. This tests validates that all of the deleted space is
+     * re-acquired. Specifically a slot not at the end or beginning in the
+     * slot directory such that edge cases are roughly checked.
+     */
     constexpr int number_frames = 1;
     PageBufferManager page_buffer_man(number_frames);
 
@@ -276,4 +302,64 @@ TEST(PageBufferManagerTest, TestFlushPageDeletedInnerSlot)
 
     std::size_t reclaimed_space = data_size * deleted_slots;
     ASSERT_EQ(page.GetFreeSpaceSize(), free_space_size + reclaimed_space);
+}
+
+
+TEST(PageBufferManagerTest, TestVacuumPageMiddleInnerSlotIntegrity)
+{
+    /* When slots are deleted and then a flush occurs the slots preceding
+     * the deleted slot will be shifted over to re-acquire the deleted slot's
+     * space. This test insures that the data stays intact after being
+     * shifted by the vacuum operation. */
+    constexpr int number_frames = 1;
+    PageBufferManager page_buffer_man(number_frames);
+
+    page_id_t page_id = page_buffer_man.NewPage();
+    auto page = page_buffer_man.WritePage(page_id);
+
+    /* allocate slots in page */
+    constexpr int num_slots = 3;
+    constexpr int data_size = 4;
+    const std::vector<char> slot1_data(data_size, 'a');
+    const std::vector<char> slot3_data(data_size, 'b');
+
+
+    auto slot1 = page.AllocateSlot(data_size);
+    ASSERT_TRUE(slot1.has_value());
+    page.WriteSlot(slot1.value(),  slot1_data);
+
+    auto slot2 = page.AllocateSlot(data_size);
+    ASSERT_TRUE(slot2.has_value());
+
+    auto slot3 = page.AllocateSlot(data_size);
+    ASSERT_TRUE(slot3.has_value());
+    page.WriteSlot(slot3.value(),  slot3_data);
+
+    EXPECT_EQ(page.GetNumSlots(), 3);
+
+    offset_t free_space_size = page.GetFreeSpaceSize();
+
+    /* delete slots in the center */
+    page.DeleteSlot(slot2.value());
+    EXPECT_EQ(page.GetNumSlots(), 2);
+
+    /* clean up deleted slots */
+    page.VacuumPage();
+
+    std::size_t reclaimed_space = data_size;
+    EXPECT_EQ(page.GetFreeSpaceSize(), free_space_size + reclaimed_space);
+
+
+    /* check the integrity of the shifted slots post flush operation */
+    auto slot1_read_span = page.ReadSlot(slot1.value());
+    EXPECT_TRUE(std::equal(
+                slot1_data.begin(), slot1_data.end(),
+                slot1_read_span.begin(), slot1_read_span.end())
+            );
+
+    auto slot3_read_span = page.ReadSlot(slot3.value());
+    EXPECT_TRUE(std::equal(
+                slot3_data.begin(), slot3_data.end(),
+                slot3_read_span.begin(), slot3_read_span.end())
+            );
 }
