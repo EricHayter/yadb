40edbe738e2a7fc2a0f4a28012752be4860e0048
Eric <hayter.eric@gmail.com>
2025-10-24 16:07:05 -0400

    Design first interface for B+ Tree

---
+++ include/storage/bptree/tree/bptree.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "buffer/page_buffer_manager.h"
+#include "page/page.h"
+
+class BPTree {
+public:
+    /* Creates an entirely new BPTree */
+    BPTree(PageBufferManager* page_buffer_manager);
+
+    /* Get a handle to an existing B+ Tree */
+    BPTree(PageBufferManager* page_buffer_manager, page_id_t root_page_id);
+
+    std::optional<std::span<const char>> Search(std::span<const char> key);
+    void Insert(std::span<const char> key, std::span<const char> value);
+    void Delete(std::span<const char> key);
+
+private:
+    page_id_t root_page_id__m;
+    PageBufferManager* page_buffer_manager_m;
+};
