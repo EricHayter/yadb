9a11de628986ab04b7c369b5319c178c58fd553e
Eric <hayter.eric@gmail.com>
2025-10-26 18:59:25 -0400

    Remove root page type and create record_id_t type

--- include/storage/bptree/page/page_layout.h
+++ include/storage/bptree/page/page_layout.h
@@ -84,6 +84,17 @@ using slot_id_t = uint16_t;
 /* offset into page */
 using offset_t = uint16_t;
 
+/**
+ * Unique identifier for a record in the database
+ *
+ * Combines page_id and slot_id to uniquely identify a tuple.
+ * Packed to ensure deterministic serialization (6 bytes total).
+ */
+struct record_id_t {
+    page_id_t page_id;
+    slot_id_t slot_id;
+} __attribute__((packed));
+
 /* Size of ALL pages in the database. Maximum allowable value of 65536 due
  * to the constraints on definitions of offset and slot_id types */
 constexpr std::size_t PAGE_SIZE = 4096;
@@ -93,9 +104,8 @@ using PageView = std::span<const char, PAGE_SIZE>;
 
 enum class PageType : uint8_t {
     Data = 0x0,
-    BPTreeRoot = 0x1,
-    BPTreeInner = 0x2,
-    BPTreeLeaf = 0x3,
+    BPTreeInner = 0x1,
+    BPTreeLeaf = 0x2,
 };
 
 /*-----------------------------------------------------------------------------
