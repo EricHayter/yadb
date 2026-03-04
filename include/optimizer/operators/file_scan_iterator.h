9faab8e63f5783f1564b5630e933e38a3a09c66f
Eric <hayter.eric@gmail.com>
2026-03-04 06:47:43 -0500

    Implement file scan iterator

--- include/optimizer/operators/file_scan_iterator.h
+++ include/optimizer/operators/file_scan_iterator.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include "optimizer/operators/iterator.h"
-#include "table/table.h"
+#include "table/table_iterator.h"
+#include <memory>
 
 class FileScanIterator : public Iterator {
     public:
-    FileScanIterator(Table& table);
+    FileScanIterator(std::unique_ptr<TableIterator> table_iter) : table_iter_m(std::move(table_iter)) {};
     ~FileScanIterator() override;
     std::optional<std::vector<std::byte>> next() override;
     private:
-    Table& table_m;
+    std::unique_ptr<TableIterator> table_iter_m;
     bool is_closed_m = false;
 };
--- src/optimizer/operators/file_scan_iterator.cpp
+++ src/optimizer/operators/file_scan_iterator.cpp
@@ -1,1 +1,11 @@
 #include "optimizer/operators/file_scan_iterator.h"
+#include <optional>
+
+std::optional<std::vector<std::byte>> FileScanIterator::next() {
+    auto row = table_iter_m->next();
+    if (!row.has_value())
+        return std::nullopt;
+    auto [rid, row_data] = row.value();
+    std::vector<std::byte> res(row_data.begin(), row_data.end());
+    return res;
+}
