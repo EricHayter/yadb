c6706e566bdbeee1b939c660a5eaf43499ca91bf
Eric <hayter.eric@gmail.com>
2026-03-14 18:16:48 -0400

    Implement close method on file scan iterator

--- include/optimizer/operators/file_scan_iterator.h
+++ include/optimizer/operators/file_scan_iterator.h
@@ -5,11 +5,13 @@
 #include <memory>
 
 class FileScanIterator : public Iterator {
-    public:
+public:
     FileScanIterator(std::unique_ptr<TableIterator> table_iter) : table_iter_m(std::move(table_iter)) {};
     ~FileScanIterator() override;
     std::optional<std::vector<std::byte>> next() override;
-    private:
+    void close() override;
+
+private:
     std::unique_ptr<TableIterator> table_iter_m;
     bool is_closed_m = false;
 };
--- src/optimizer/operators/file_scan_iterator.cpp
+++ src/optimizer/operators/file_scan_iterator.cpp
@@ -1,6 +1,12 @@
 #include "optimizer/operators/file_scan_iterator.h"
 #include <optional>
 
+FileScanIterator::~FileScanIterator() {
+    if (!is_closed_m) {
+        table_iter_m->close();
+    }
+}
+
 std::optional<std::vector<std::byte>> FileScanIterator::next() {
     auto row = table_iter_m->next();
     if (!row.has_value())
@@ -8,4 +14,11 @@ std::optional<std::vector<std::byte>> FileScanIterator::next() {
     auto [rid, row_data] = row.value();
     std::vector<std::byte> res(row_data.begin(), row_data.end());
     return res;
+}
+
+void FileScanIterator::close() {
+    if (!is_closed_m) {
+        table_iter_m->close();
+        is_closed_m = true;
+    }
 }
